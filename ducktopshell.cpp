719dcdbd82f3febd91f6cb8fa3c9f0c14a027fbe
Erik Inkinen <erik.inkinen@gmail.com>
2021-07-05 13:16:04 +0300

    Unset qt generic plugins variable

--- ducktopshell.cpp
+++ ducktopshell.cpp
@@ -34,6 +34,7 @@ void DucktopShell::execApp(QString command)
 {
     qputenv("QT_QPA_PLATFORM", QByteArray("wayland"));
     qunsetenv("QT_IM_MODULE");
+    qunsetenv("QT_QPA_GENERIC_PLUGINS");
     qputenv("QT_SCALE_FACTOR", this->engine.rootContext()->contextProperty("shellScaleFactor").toString().toUtf8());
     qputenv("WAYLAND_DISPLAY", wsocketname);
     QStringList args = QStringList();
